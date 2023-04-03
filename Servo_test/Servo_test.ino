3864a0da710e920721cc9a65da5b0fed931f9d87
Cippppy <98924381+Cippppy@users.noreply.github.com>
2023-04-03 11:11:42 -0400

    Changes

--- Servo_test/Servo_test.ino
+++ Servo_test/Servo_test.ino
@@ -1,15 +1,30 @@
+/*
+ * Part3.c
+ *
+ *  Created on: Feb 22, 2023
+ *  Edited on: Feb 28, 2023
+ *      Author: Christian Cipolletta
+ *
+ *  This code controls the angle of a servo by generating a hardware PWM.
+ *  The angle increased with the press of one button and is decreased with the press of another.
+ */
+
 #include <msp430.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  WDTCTL = WDTPW | WDTHOLD;                 // Stop WDT
-  pinMode(P4_6,OUTPUT); // make the LED pin output
-  pinMode(P6_6,OUTPUT);
-  P4SEL0 |= BIT6;
-  P4SEL1 &= ~BIT6;
-  digitalWrite(P4_6,LOW);
-  digitalWrite(P6_6,LOW);
-  setupTimer(20000); // set timer period to 20000 micro seconds  
+void ButtonInit();      // Method to initialize buttons
+void ServoInit();       // Method to initialize servo pin
+void AlarmInit();
+void TimerB3Init();     // Method to initialize TimerB3 which does the duty cycle
+
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;                 // Stop WDT
+    ButtonInit();               // Method to initialize buttons
+    ServoInit();                // Method to initialize servo pin
+    TimerB3Init();              // Method to initialize TimerB3 which does the duty cycle
+
+    // Disable the GPIO power-on default high-impedance mode to activate
+    // previously configured port settings
     PM5CTL0 &= ~LOCKLPM5;
 
     __bis_SR_register(LPM0_bits + LPM3_bits + GIE);             // Enter LPM0 and LPM3
@@ -17,49 +32,88 @@ void setup() {
     __no_operation();                         // For debugger
 }
 
-void loop() {
-  // put your main code here, to run repeatedly: 
+
+/*
+ * INITIALIZATION METHODS
+ */
+
+// Method to initialize buttons
+void ButtonInit() {
+    pinMode(P4_1, INPUT_PULLUP);                // Initializes Pin 4.1 (button) as an input
+    P4IES |= BIT1;                  // P4.1 High --> Low edge
+    P4IE |= BIT1;                   // P4.1 interrupt enabled
+
+    pinMode(P2_3,INPUT_PULLUP);                // Initializes Pin 2.3 (button) as an input
+    P2IES |= BIT3;                 // P2.3 High --> Low edge
+    P2IE |= BIT3;                   // P2.3 interrupt enabled
 }
 
-//void OnTimer()
-//{
-//  static long msCount=0;// Count milliseconds to allow a 1 second pulse
-//  static int state=0;  // Remember LED state for toggling purposes
-// 
-//  msCount++;
-//  if (msCount >= 1.5)
-//  {
-//    msCount = 0;
-//    digitalWrite(P6_6,state); // Write to LED
-//    state=~state;             // toggle state
-//  }
-//}
+// Method to initialize servo pin
+void ServoInit() {
 
-void setupTimer(unsigned Period)
-{
-  // Configuration word
-  // Bits 15-10: Unused
-  // Bits 9-8: Clock source select: set to SMCLK (1MHz)
-  // Bits 7-6: Input divider: set to 0
-  // Bits 5-4: Mode control: Count up to TBCCRO and reset
-  // Bit 3: Unused
-  // Bit 2: TBCLR : set to initially clear timer system
-  // Bit 1: Enable interrupts from TB0
-  // Bit 0: Interrupt (pending) flag : set to zero (initially)
+    P6DIR |= BIT0;                     // P6.0 output
+    P6SEL0 |= BIT0;
+    P6SEL1 &= ~BIT0;                    // P6.0 options select
+}
+
+void AlarmInit(){
+    P6DIR |= BIT1;                
+    P6SEL0 |= BIT1;
+    P6SEL1 &= ~BIT1;
+
+    P6DIR |= BIT2;
+    P6SEL0 |= BIT2;
+    P6SEL1 &= ~BIT2;
+}
+
+// Method to initialize TimerB3 which does the duty cycle
+void TimerB3Init() {
     // Configure Timer_B3
-    TB0CCR0 = 20000;                         // PWM Period of 50Hz
+    TB3CCR0 = 20000;                         // PWM Period of 50Hz
 
-    TB0CCTL1 = OUTMOD_7;                      // CCR1 reset/set
-    TB0CCR1 = 20000*0.075;                   // CCR1 PWM duty cycle
+    TB3CCTL1 = OUTMOD_7;                      // CCR1 reset/set
+    TB3CCR1 = 20000*0.075;                   // CCR1 PWM duty cycle
+    TB3CCR2 = 20000;
+    TB3CCR3 = 20000;
 
-    TB0CTL = TBSSEL__SMCLK | MC__UP | TBCLR;  // SMCLK, up mode, clear TBR
+    TB3CTL = TBSSEL__SMCLK | MC__UP | TBCLR;  // SMCLK, up mode, clear TBR
 }
 
-//// The address function that follows this vector statement is placed in the specified location Interrupt Vector table 
-//#pragma vector=TIMER0_B0_VECTOR
-//__interrupt  void timerB0ISR(void)
-//{
-//// Timer B0 Interrupt service routine
-//  OnTimer();
-//  TB0CTL &= ~BIT0;     // Acknowledge the interrupt      
-//}
+
+/*
+ * INTERRUPT ROUTINES
+ */
+
+// Port 2 interrupt service routine
+#pragma vector=PORT2_VECTOR
+__interrupt void Port_2(void)
+{
+    P2IFG &= ~BIT3;                         // Clear P2.3 IFG
+    if(TB3CCR1 >= 20000*0.125) {            // If duty cycle is max, stay at max
+        TB3CCR1 = 20000*0.125;
+        TB3CCR2 = 0;
+        TB3CCR3 = 0;
+    }
+    else {
+        TB3CCR1 += 100;                     // Else increase duty cycle by 10%
+        TB3CCR2 -= 1000;
+        TB3CCR3 -= 1000;
+    }
+}
+
+// Port 4 interrupt service routine (For reset button)
+#pragma vector=PORT4_VECTOR
+__interrupt void Port_4(void)
+{
+    P4IFG &= ~BIT1;                         // Clear P4.1 IFG
+    if(TB3CCR1 <= 20000*0.03) {             // If duty cycle is at min, stay at min
+        TB3CCR1 = 20000*0.03;
+        TB3CCR2 = 20000;
+        TB3CCR3 = 20000;
+    }
+    else {
+        TB3CCR1 -= 100;                     // Else decrease duty cycle by 10%
+        TB3CCR2 += 1000;
+        TB3CCR3 += 1000;
+    }
+}
--- buzzer_test/buzzer_test.ino
+++ buzzer_test/buzzer_test.ino
@@ -1,39 +1,83 @@
-unsigned long TP2_6 = 0.384615385; // Time period for 2.6kHz
-unsigned long halfTP2_6 = TP2_6; // 50% Duty Cycle for 2.6kHz
+#include <msp430.h>
 
-unsigned long TP2_8 = 0.357142857; // Time period for 2.8kHz
-unsigned long halfTP2_8 = TP2_8; // 50% Duty Cycle for 2.8kHz
+void BuzzerInit();
+void TimerB1Init();
 
-unsigned long TP2_4 = 0.416666667; // Time period for 2.4kHz
-unsigned long halfTP2_4 = TP2_4; // 50% Duty Cycle for 2.4kHz
+int main(void)
+{
+  WDTCTL = WDTPW | WDTHOLD;                 // Stop WDT
+  BuzzerInit();
+  TimerB1Init();
 
-unsigned char state = 1; // Sets which frequency torun at for testing purposes
+    // Disable the GPIO power-on default high-impedance mode to activate
+    // previously configured port settings
+    PM5CTL0 &= ~LOCKLPM5;
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(P1_2,OUTPUT); // Squarewave output 1.2
+    __bis_SR_register(LPM0_bits + LPM3_bits + GIE);             // Enter LPM0 and LPM3
+    __enable_interrupt();                               // Enables interrupts
+    __no_operation();                         // For debugger
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  switch(state){
-    case 0 : // square wave at 2.6kHz
-      digitalWrite(P1_2, HIGH);
-      delay(halfTP2_6);
-      digitalWrite(P1_2,LOW);
-      delay(halfTP2_6);
-      break;
-    case 1 : // square wave at 2.8kHz
-      digitalWrite(P1_2, HIGH);
-      delay(halfTP2_8);
-      digitalWrite(P1_2,LOW);
-      delay(halfTP2_8);
-      break;
-    case 2 : // square wave at 2.4kHz
-      digitalWrite(P1_2, HIGH);
-      delay(halfTP2_6);
-      digitalWrite(P1_2,LOW);
-      delay(halfTP2_6);
-      break;
-  }
+// Method to initialize Buzzer pin
+void BuzzerInit() {
+    P2DIR |= BIT0;                     // P6.0 output
+    P2SEL0 |= BIT0;
+    P2SEL1 &= ~BIT0;                    // P6.0 options select
 }
+
+// Method to initialize TimerB1 which does the duty cycle
+void TimerB1Init() {
+    // Configure Timer_B1
+    TB1CCR0 = 384.615384615;                         // PWM Period of 50Hz
+
+    TB1CCTL1 = OUTMOD_7;                      // CCR1 reset/set
+    TB1CCR1 = 384.615384615/2;                   // CCR1 PWM duty cycle
+
+    TB1CTL = TBSSEL__SMCLK | MC__UP | TBCLR;  // SMCLK, up mode, clear TBR
+}
+
+
+
+/*
+ * With Delays
+ */
+
+//unsigned long TP2_6 = 0.384615385; // Time period for 2.6kHz
+//unsigned long halfTP2_6 = TP2_6; // 50% Duty Cycle for 2.6kHz
+//
+//unsigned long TP2_8 = 0.357142857; // Time period for 2.8kHz
+//unsigned long halfTP2_8 = TP2_8; // 50% Duty Cycle for 2.8kHz
+//
+//unsigned long TP2_4 = 0.416666667; // Time period for 2.4kHz
+//unsigned long halfTP2_4 = TP2_4; // 50% Duty Cycle for 2.4kHz
+//
+//unsigned char state = 1; // Sets which frequency torun at for testing purposes
+//
+//void setup() {
+//  // put your setup code here, to run once:
+//  pinMode(P1_2,OUTPUT); // Squarewave output 1.2
+//}
+//
+//void loop() {
+//  // put your main code here, to run repeatedly:
+//  switch(state){
+//    case 0 : // square wave at 2.6kHz
+//      digitalWrite(P1_2, HIGH);
+//      delay(halfTP2_6);
+//      digitalWrite(P1_2,LOW);
+//      delay(halfTP2_6);
+//      break;
+//    case 1 : // square wave at 2.8kHz
+//      digitalWrite(P1_2, HIGH);
+//      delay(halfTP2_8);
+//      digitalWrite(P1_2,LOW);
+//      delay(halfTP2_8);
+//      break;
+//    case 2 : // square wave at 2.4kHz
+//      digitalWrite(P1_2, HIGH);
+//      delay(halfTP2_6);
+//      digitalWrite(P1_2,LOW);
+//      delay(halfTP2_6);
+//      break;
+//  }
+//}
