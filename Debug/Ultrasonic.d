7d569935855c94b7c652a1bfae4b61c6c69b827a
Cippppy <98924381+Cippppy@users.noreply.github.com>
2023-04-09 19:00:41 -0400

    Made CCS version

---
+++ Debug/Ultrasonic.d
@@ -0,0 +1,30 @@
+# FIXED
+
+Ultrasonic.obj: ../Ultrasonic.c
+Ultrasonic.obj: C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430.h
+Ultrasonic.obj: C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430fr2355.h
+Ultrasonic.obj: C:/ti/ccs1220/ccs/ccs_base/msp430/include/in430.h
+Ultrasonic.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+Ultrasonic.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+Ultrasonic.obj: C:/ti/ccs1220/ccs/ccs_base/msp430/include/legacy.h
+Ultrasonic.obj: C:/Users/Cip/Documents/School/Semester\ 4/Embedded/lab-1-gpio-demo-and-driver-Cippppy/4\ GPIO\ Driver/GPIO_Driver.h
+Ultrasonic.obj: C:/Users/Cip/Documents/School/Semester\ 4/Embedded/UARTDriver/FR2355_UART_Driver.h
+
+../Ultrasonic.c:
+
+C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430fr2355.h:
+
+C:/ti/ccs1220/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ti/ccs1220/ccs/ccs_base/msp430/include/legacy.h:
+
+C:/Users/Cip/Documents/School/Semester\ 4/Embedded/lab-1-gpio-demo-and-driver-Cippppy/4\ GPIO\ Driver/GPIO_Driver.h:
+
+C:/Users/Cip/Documents/School/Semester\ 4/Embedded/UARTDriver/FR2355_UART_Driver.h:
+
---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr2355.cmd 
+
+C_SRCS += \
+../Ultrasonic.c 
+
+C_DEPS += \
+./Ultrasonic.d 
+
+OBJS += \
+./Ultrasonic.obj 
+
+OBJS__QUOTED += \
+"Ultrasonic.obj" 
+
+C_DEPS__QUOTED += \
+"Ultrasonic.d" 
+
+C_SRCS__QUOTED += \
+"../Ultrasonic.c" 
+
+
